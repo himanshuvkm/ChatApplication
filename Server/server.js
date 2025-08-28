e9d31b62ed8418c75e1be2991d6384f927c471e1
himanshuvkm <himanshuvkm252@gmail.com>
2025-08-28 10:08:50 +0530

    completed

--- Server/server.js
+++ Server/server.js
@@ -1,32 +1,36 @@
 import path from "path";
 import express from "express";
-import dotenv  from "dotenv";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import authRoute from "./Routes/auth-route.js"
-import messageRoute from "./Routes/message-routes.js"
+import authRoute from "./Routes/auth-route.js";
+import messageRoute from "./Routes/message-routes.js";
 import connecttoDb from "./DB/connecttoDb.js";
 import userRoute from "./Routes/user-route.js";
-import {app,server} from "./Socket/socket.io.js"
-dotenv.config()
-// const app = express(); 
-const __dirname = path.resolve()
+import { app, server } from "./Socket/socket.io.js";
 
-const Port = process.env.PORT
+dotenv.config();
+const __dirname = path.resolve();
+const Port = process.env.PORT;
 
-app.use(express.json())
+// Middlewares
+app.use(express.json());
 app.use(cookieParser());
 
+// DB connect
 connecttoDb();
-app.use('/api/auth', authRoute)
-app.use('/api/message', messageRoute)
-app.use('/api/users', userRoute)
 
-app.use(express.static(path.join(__dirname, '../Client/dist')))
+// API routes
+app.use("/api/auth", authRoute);
+app.use("/api/message", messageRoute);
+app.use("/api/users", userRoute);
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../Client/dist/index.html'));
-// });
+// Serve frontend
+app.use(express.static(path.join(__dirname, "../Client/dist")));
 
-server.listen(Port , ()=>{
-    console.log(`Server is running on port ${Port}`);
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Client/dist/index.html"));
+});
+
+server.listen(Port, () => {
+  console.log(`Server is running on port ${Port}`);
+});
